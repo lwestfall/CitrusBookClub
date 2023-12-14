ab1eb68d87a68ef68e9238bfa45a2969a447e41c
Luke Westfall <luke@ljdub.com>
2023-12-13 21:25:11 -0500

    fix: adjust urls

--- src/Client/src/environments/environment.staging.ts
+++ src/Client/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.ljdub.com/cbc-test',
+  apiBaseUrl: 'https://cbc-test-api.ljdub.com',
   googleClientId:
     '1019710498366-63edfol2ig1nmjiqh5lto3a9kaj33786.apps.googleusercontent.com',
 };
